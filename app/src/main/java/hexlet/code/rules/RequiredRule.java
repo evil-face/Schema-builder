f941c5ea81fbaa283ff69d18b4974a95a070f4eb
evil_face <den.bragin@gmail.com>
2023-03-26 17:07:49 +0600

    delete RequiredRule.java

--- app/src/main/java/hexlet/code/rules/RequiredRule.java
+++
@@ -1,8 +0,0 @@
-package hexlet.code.rules;
-
-public final class RequiredRule implements IRule {
-    @Override
-    public boolean validate(Object o) {
-        return o instanceof String s && !s.isEmpty();
-    }
-}
