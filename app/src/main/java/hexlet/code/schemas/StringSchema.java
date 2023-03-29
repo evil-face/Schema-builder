c5b4d523dda258bd36cd749c2f0d6c7c1981e75e
evil_face <den.bragin@gmail.com>
2023-03-29 13:28:17 +0600

    refactor: improved predicate logic

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -6,18 +6,18 @@ public final class StringSchema extends BaseSchema {
     public StringSchema required() {
         setIsRequiredOn();
         Predicate<Object> required = o -> o instanceof String s && !s.isEmpty();
-        addRule(required);
+        addFirstRule(required);
         return this;
     }
 
     public StringSchema minLength(int length) {
-        Predicate<Object> minLengthRule = o -> o instanceof String s && s.length() >= length;
+        Predicate<Object> minLengthRule = o -> ((String) o).length() >= length;
         addRule(minLengthRule);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        Predicate<Object> contains = o -> o instanceof String s && s.contains(substring);
+        Predicate<Object> contains = o -> ((String) o).contains(substring);
         addRule(contains);
         return this;
     }
